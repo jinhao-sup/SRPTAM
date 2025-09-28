b073a8e90974f6de0797d8e985a4a2e46fe68912
jinhao-sup <57715868+jinhao-sup@users.noreply.github.com>
2025-09-29 05:00:04 +0800

    Update configuration.py

--- utils/configuration.py
+++ utils/configuration.py
@@ -3,10 +3,6 @@ import os
 from utils.error_msg import Error
 
 
-""" 
- 加载给出路径的配置文件，即configs中的3个配置文件，从程序执行指令中指定， 包含对数据集，训练超参，测试超参，cache还不清楚作用，日志配置等
- 区分kcore中的默认加载ml1m的配置，这里从指令中给出
-"""
 def load_configuration(descriptor):  # 传入路径
     """
     Load configuration from the given descriptor.
